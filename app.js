17c54f093ebca5caaa233ad7be2bdc51e48d6420
Freddy Karam Azmy <karam-azmy@elsoft.dev>
2026-09-25 21:55:10 +0200

    Add Plesk Node start command

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Browser entry for Vite; Plesk/Node loads the server only at runtime.
 if (typeof process !== 'undefined' && process.versions?.node) {
-  await import(/* @vite-ignore */ './server/node-server.js')
+  import(/* @vite-ignore */ './server/node-server.js').catch((error) => {
+    console.error('DUMAH server failed to start', error)
+    process.exitCode = 1
+  })
 } else {
-  await import('./src/main.js')
+  import('./src/main.js')
 }
